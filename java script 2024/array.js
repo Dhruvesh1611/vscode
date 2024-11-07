b1afc65bd437963e39b6c31b64bc4364fb212a68
Dhruvesh1611 <dhruvesh.shyara.cg@gmail.com>
2024-11-07 13:44:42 +0530

    Update array.js

--- java script 2024/array.js
+++ java script 2024/array.js
@@ -1,20 +1,3 @@
 
 
-// let Array_name = ["Banana",16, "Orange",function(){}, "Apple", "Mango"];
-
-//FOR ARRAY ELEMENTS
-// Array_name
-
-// //FOR ARRAY LENGTH
-// Array_name.length
-
-// //FOR replace this "[]" to this ".at()"
-// Array_name.at(4)
-
-// //FOR JOIN ALL THE ELEMENTS OF ARRAY
-// Array_name.join("*")
-
-// //FOR JOIN ALL THE ELEMENTS OF ARRAY
-// Array_name.POP()
-
-console.log(Array_name[4])
+helllo dosto
